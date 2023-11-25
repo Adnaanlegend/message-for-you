32636433aa219889c9b37af0cfe0e035dbb50e11
Adnaan Hussain <92253624+Adnaanlegend@users.noreply.github.com>
2023-11-25 20:36:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Can't wait, 8th December";
+  question.innerHTML = "Can't wait, mi amor!";
   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
 
